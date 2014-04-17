671705eb99d5904a87d73d75e1b42e0b7f3fdd50
Julien Murtin <jul.murtin@gmail.com>
2014-04-17 19:40:57 +0900

    Fleeing Sheeps !!

--- GW2014_Muffins/src/js/character_log.js
+++ GW2014_Muffins/src/js/character_log.js
@@ -1,15 +1,24 @@
-﻿define(['sceneManager', 'tileCollider', 'log'], function (sceneManager, TileCollider, Log) {
-    return function CharacterLOG(x, y) {
+﻿define(['sceneManager', 'tileCollider', 'log', 'eventManager'], function (sceneManager, TileCollider, Log, eventManager)
+{
+    return function CharacterLOG(x, y)
+    {
+        var gameScene = sceneManager.currentScene;
+
         var wolf =
         {
             ID: 0,
             x: x,
             y: y,
-            yOffset: 0,
-            width: 184,
+            yOffset: -gameScene.groundOffset,
+
+            width: 92,
             height: 92,
+
             speedY: 0,
             speedX: 6,
+
+            dir: 0,
+            jump: false,
             impulsion: 8,
 
             anim: 0,
@@ -24,6 +33,29 @@
             grounded: false,
             collider: null
         };
+
+        eventManager.Add('LEFT_DOWN', moveLeft);
+        eventManager.Add('LEFT_UP', moveRight);
+
+        eventManager.Add('RIGHT_DOWN', moveRight);
+        eventManager.Add('RIGHT_UP', moveLeft);
+
+        eventManager.Add('JUMP_UP', moveJump);
+
+        function moveLeft()
+        {
+            wolf.dir += 1;
+        }
+
+        function moveRight()
+        {
+            wolf.dir -= 1;
+        }
+
+        function moveJump()
+        {
+            wolf.jump = true;
+        }
 
         function move() {
             wolf.grounded = false;
@@ -51,22 +83,22 @@
                         wolf.speedY = 0;
                         // Le cube est replacé au niveau du sol
                         wolf.y = gameScene.mapP.wallground[i].y - gameScene.mapP.wallground[i].height / 2 - wolf.height / 2 + 1;
-
                     }
                 };
             }
 
             //déplacement du vaisseau
-            if (Input.jump == true && wolf.grounded) {
+
+            if (wolf.jump == true && wolf.grounded) {
                 wolf.speedY = -wolf.impulsion;
-                wolf.grounded = false;
+                wolf.grounded = wolf.jump = false;
             }
 
-            wolf.speedY += wolf.grounded ? 0 : gravity;
+            wolf.speedY += wolf.grounded ? 0 : .8;//gravity;
             wolf.y += wolf.speedY;
 
             var x = wolf.x;
-            wolf.x += (Input.rightKey - Input.leftKey) * wolf.speedX;
+            wolf.x += /*(Input.rightKey - Input.leftKey)*/ wolf.dir * wolf.speedX;
             for (var i = 0; i < gameScene.mapP.wallground.length; i++) {
                 if (wolf.collider.CheckGround(gameScene.mapP.wall[i])) {
                     wolf.x = x;
@@ -136,7 +168,7 @@
         };
 
         function update(dt) {
-
+            move();
         };
 
         return {
